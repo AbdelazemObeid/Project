806287eb54c7cf2daae6f7a82fa10dd2cef485ba
fsio33 <bydbdalzym002@gmail.com>
2026-05-05 00:28:10 +0300

    clean project remove vs files

--- project MVC/Controllers/user/HomeController.cs
+++ project MVC/Controllers/user/HomeController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using project_MVC.data;
 
 namespace project_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/")]
         public IActionResult Index()
         {
-            return View();
+            return View("~/views/user/home/home.cshtml");
         }
     }
 }
