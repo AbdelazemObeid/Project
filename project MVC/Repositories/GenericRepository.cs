3c07de18adfd63ac47515ed32b13d2afebdcc544
fsio33 <bydbdalzym002@gmail.com>
2026-05-14 23:16:16 +0300

    update project

---
+++ project MVC/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using project_MVC.data;
+
+namespace project_MVC.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly Project_context context;
+
+        public GenericRepository(Project_context _context)
+        {
+            context = _context;
+        }
+        public void add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public void delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public List<T> getAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T getById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void save()
+        {
+            context.SaveChanges();
+        }
+
+        public void update(T entity)
+        {
+            context.Set<T>().Update(entity);
+        }
+    }
+}
