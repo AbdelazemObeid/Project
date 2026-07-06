36b45219536d2817edddc034c83d2a066241eda5
Abdoelazem Obeid <bydbdalzym002@gmail.com>
2026-07-06 21:52:52 +0300

    Delete project MVC directory

--- project MVC/Repositories/IUserReposatory.cs
+++
@@ -1,9 +0,0 @@
-﻿using project_MVC.Models;
-
-namespace project_MVC.Repositories
-{
-    public interface IUserReposatory : IGenericRepository<User>
-    {
-        Boolean getByEmail(string email);
-    }
-}
