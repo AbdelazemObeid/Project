6b74f1bbe79e66a35aadd0f79b5f26cbb0642853
fsio33 <bydbdalzym002@gmail.com>
2026-07-06 20:38:04 +0300

    add new connection string on host

---
+++ project MVC/Migrations/20260706171809_add-new-databasev0.1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnewdatabasev01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
